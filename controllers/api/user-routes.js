dab0a1574be3e8f0d427a7aedff27a985560ea83
Maya N <nettathm@gmail.com>
2023-10-31 21:58:39 -0600

    added login and signup post routes to user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 
-router.get("/", async (req, res) => {
+// http://localhost:3001/api/users/login
+router.post("/login", async (req, res) => {
     try {
-      res.status(200).json("This is the home route!");
+      res.status(200).json("This action will check to see if the users login credentials are valid!");
     } catch (error) {
       console.log(err);
       res.status(500).json(err);
     }
   });
-  
+
+  // http://localhost:3001/api/users/signup
+router.post("/signup", async (req, res) => {
+    try {
+      res.status(200).json("This action will create a new user!");
+    } catch (error) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
