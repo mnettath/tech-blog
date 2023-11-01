d4466bad9e8ae4c2640623ac3344ebdd82df6ffa
Maya N <nettathm@gmail.com>
2023-10-31 21:44:25 -0600

    able to launch server, fixed bugs

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get("/", async (req, res) => {
+    try {
+      res.status(200).json("This is the home route!");
+    } catch (error) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+module.exports = router;
