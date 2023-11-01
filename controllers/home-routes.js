f581ea2f000e91009a6182b2e012acdb41c72a61
Maya N <nettathm@gmail.com>
2023-10-31 22:41:35 -0600

    added handlebars, verified that homepage is rendered through appropriate route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // http://localhost:3001
 router.get("/", async (req, res) => {
   try {
-    res.status(200).json("This is the home route!");
+    res.render("homepage");
   } catch (error) {
     console.log(err);
     res.status(500).json(err);
