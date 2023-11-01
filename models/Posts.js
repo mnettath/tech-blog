e91addc38befa72c807f7af6b0658ac2d4f132f0
Maya N <nettathm@gmail.com>
2023-11-01 17:39:52 -0600

    added to Post model, now has all necessary components

--- models/Posts.js
+++ models/Posts.js
@@ -1,19 +1,21 @@
 const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
-Post.init({
+Post.init(
+  {
     title: {
-        type: DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     contents: {
-        type: DataTypes.STRING,
+      type: DataTypes.STRING,
     },
     dateCreated: {
-        type: DataTypes.DATE,
+      type: DataTypes.DATE,
     },
-})
+  },
+  { sequelize, freezeTableName: true, underscored: true, modelName: "post" }
+);
 
 module.exports = Post;
